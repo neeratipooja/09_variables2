ebbbca8fa57357d1c11b786d33e4d7bd0b47c439
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-10-28 22:14:40 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,21 @@
+'''
+DATE=27th oct 2022
+DAY= thursday
+TOPIC=variables 
+AUTHOR= KRISHNA POOJA
+'''
+a=10
+_b=20
+A=60
+c_4f=50
+z5=100
+print(a,_b,A,c_4f,z5)   #10 20 60 50 100
+print('pooja'.isidentifier()) #true
+print('3pooja'.isidentifier()) #false
+print('_pooja'.isidentifier()) #true
+print('hello poo\'ja') #hello poo'ja
+print("hello poo'ja") #hello poo'ja
+print('poojakrishna '*5)
+print('''abcdefghijklmnopqrstuvwxyzmmmmmmmmmmmmmmmmmmmmmmmmmmmmmvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvmkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+hhhhhhhhhhhhh ''')
+
